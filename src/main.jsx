aa0e802d8acaae71dcf23859ecb2a6a7aec903c3
AniruddhaMJois <aniruddhamanjunathajois1tab@gmail.com>
2026-05-29 12:28:21 +0530

    Fix React crash (DOMException) caused by Google Translate widget modifying DOM nodes during navigation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Fix React crashing when Google Translate modifies the DOM (React DOMException during navigation)
+if (typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function(child) {
+    if (child.parentNode !== this) {
+      if (console) {
+        console.warn('Cannot remove a child from a different parent (Google Translate mutation)', child, this);
+      }
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments);
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function(newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (console) {
+        console.warn('Cannot insert before a reference node from a different parent (Google Translate mutation)', referenceNode, this);
+      }
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments);
+  };
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
